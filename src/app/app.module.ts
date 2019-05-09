c1d989730e535e1736ccf27274d64176b18311df
Noel Bor <NoelkBor@gmail.com>
2019-05-09 20:21:34 +0300

    update on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
 // import { BrowserModule } from '@angular/platform-browser';
 // import { NgModule } from '@angular/core';
 // import { FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
-// import { AppComponent } from './app.component';
-// import { ProfileComponent } from './profile/profile.component';
-// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
                                                                                                                                    
 
 
-// @NgModule({       
-//   declarations: [                                                                               
-//   AppComponent,
-//   ProfileComponent
+@NgModule({       
+  declarations: [                                                                               
+  AppComponent,
+  ProfileComponent
   
-// ],
+],
 
-// imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     FormsModule
+imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
 
         
 //   ],
