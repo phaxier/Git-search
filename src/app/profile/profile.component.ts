eb15f8cf60ac5d1fad77033077132a867b87c7d8
Noel Bor <NoelkBor@gmail.com>
2019-05-09 20:19:37 +0300

    create profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,33 +6,33 @@ import { ProfileService } from '../profile.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-// export class ProfileComponent implements OnInit {
-//   user: any;
-//   repos: any[];
-//   username: string;
+export class ProfileComponent implements OnInit {
+  user: any;
+  repos: any[];
+  username: string;
 
-//   constructor(private ProfileService: ProfileService) {
-//     this.user = false;
-//     this.ProfileService.getUser().subscribe(user => {
-//       this.user = user;
-//     });
-//     this.ProfileService.getRepos().subscribe(repos => {
-//         this.repos = repos;
-//     });
-//   }
+  constructor(private ProfileService: ProfileService) {
+    this.user = false;
+    this.ProfileService.getUser().subscribe(user => {
+      this.user = user;
+    });
+    this.ProfileService.getRepos().subscribe(repos => {
+        this.repos = repos;
+    });
+  }
 
-//   searchUser() {
-//     this.ProfileService.updateUser(this.username);
+  searchUser() {
+    this.ProfileService.updateUser(this.username);
 
-//     this.ProfileService.getUser().subscribe(user => {
-//         this.user = user;
-//     });
-//     this.ProfileService.getRepos().subscribe(repos => {
-//         this.repos = repos;
-//     });
-//   }
+    this.ProfileService.getUser().subscribe(user => {
+        this.user = user;
+    });
+    this.ProfileService.getRepos().subscribe(repos => {
+        this.repos = repos;
+    });
+  }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
