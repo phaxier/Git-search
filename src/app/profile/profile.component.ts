5df5c94a392bc305d8d35b2842a5e42d6fdbb005
Noel Bor <NoelkBor@gmail.com>
2019-05-09 20:18:31 +0300

    create component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,38 +1,38 @@
-// import { Component, OnInit } from '@angular/core';
-// import { ProfileService } from '../profile.service';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-export class ProfileComponent implements OnInit {
-  user: any;
-  repos: any[];
-  username: string;
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+// export class ProfileComponent implements OnInit {
+//   user: any;
+//   repos: any[];
+//   username: string;
 
-  constructor(private ProfileService: ProfileService) {
-    this.user = false;
-    this.ProfileService.getUser().subscribe(user => {
-      this.user = user;
-    });
-    this.ProfileService.getRepos().subscribe(repos => {
-        this.repos = repos;
-    });
-  }
+//   constructor(private ProfileService: ProfileService) {
+//     this.user = false;
+//     this.ProfileService.getUser().subscribe(user => {
+//       this.user = user;
+//     });
+//     this.ProfileService.getRepos().subscribe(repos => {
+//         this.repos = repos;
+//     });
+//   }
 
-  searchUser() {
-    this.ProfileService.updateUser(this.username);
+//   searchUser() {
+//     this.ProfileService.updateUser(this.username);
 
-    this.ProfileService.getUser().subscribe(user => {
-        this.user = user;
-    });
-    this.ProfileService.getRepos().subscribe(repos => {
-        this.repos = repos;
-    });
-  }
+//     this.ProfileService.getUser().subscribe(user => {
+//         this.user = user;
+//     });
+//     this.ProfileService.getRepos().subscribe(repos => {
+//         this.repos = repos;
+//     });
+//   }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-}
+// }
